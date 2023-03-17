ce722fc02220c9db78104ebd4241f4ea1fa5951b
ia popkhadze <iafofkhadze@gmail.com>
2023-03-16 17:11:40 -0700

    questions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 import Langing from "./Langing";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FormPage from "./FormPage";
+import LangingPage from "./LangingPage";
+import SubmittedApplications from "./SubmittedApplications";
 function App() {
   return (
-    <Langing />
+    <Router>
+      <LangingPage />
+      <Routes>
+        <Route path="/" element={<LangingPage />} />
+        <Route path="/FormPage" element={<FormPage />} />
+        <Route
+          path="/SubmittedApplications"
+          element={<SubmittedApplications />}
+        />
+        <Route path="*" element={<div>Nothing Found</div>} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/Langing.js
+++ src/Langing.js
@@ -1,30 +1,11 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import FormPage from './FormPage';
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FormPage from "./FormPage";
 import LangingPage from "./LangingPage";
 import SubmittedApplications from "./SubmittedApplications";
 const Langing = () => {
-
-    return (
-        <Router>
-            <LangingPage />
-            <Switch>
-                <Route exact path="/">
-                    <LangingPage />
-                </Route>
-                <Route path="/FormPage">
-                    <FormPage />
-                </Route>
-                <Route path="/SubmittedApplications">
-                    <SubmittedApplications />
-                </Route>
-                <Route path="*">
-                    {console.log('Nothing Found :(')}
-                </Route>
-            </Switch>
-        </Router>
-    );
-}
-
+  return <>asdasd</>;
+};
 
 export default Langing;
